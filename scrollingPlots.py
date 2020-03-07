ee27d757bd6ccfdc8ae8e7317a2daec8a1a73929
James Bounds <jamesbounds07@gmail.com>
2020-03-06 18:13:46 -0600

    Initial commit. Seems to be working with Matplotlib and pyserial communication.

---
+++ scrollingPlots.py
@@ -0,0 +1,60 @@
+import serial
+import time
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+#--------Initialize Serial---------
+
+serialDevice = '/dev/ttyUSB0'
+
+ser = serial.Serial(serialDevice, 9600, timeout = 1, parity = serial.PARITY_NONE)
+ser.flush()
+
+print("Waiting for data from frequency counter....")
+ser.read_all()
+ser.read_until()
+
+def serDataToFloat(serData):
+    vals = serData.decode().split('.')
+    out = float(vals[0])
+    count = -1
+    unitLabel = None
+    for v in vals[1].split(','):
+        #Check for unit
+        spaceSplit = v.split(' ')
+        
+        strWithoutUnit = spaceSplit[0]
+        if(len(spaceSplit) > 1):
+            unitLabel = spaceSplit[1].replace('\r\n','')
+        count -= len(strWithoutUnit)
+        out += 10**(count)*float(strWithoutUnit)
+    return out, unitLabel
+        
+
+#------------Plot----------------
+
+data = []
+times = []
+timeStart = time.time()
+ser.read_until()
+
+for i in range(0,3):
+    data.append(serDataToFloat(ser.read_until())[0])
+    times.append(time.time()-timeStart)
+
+fig = plt.figure()
+ax = fig.subplots(1)
+line, = ax.plot(times,data)
+
+while True:
+    recv = serDataToFloat(ser.read_until())
+    print(recv)
+    data.append(recv[0])
+    times.append(time.time()-timeStart)
+    ax.set_xlim(0,times[-1])
+    ax.set_ylim(min(data),max(data))
+    line.set_ydata(data)
+    line.set_xdata(times)
+    fig.canvas.draw()
+    fig.canvas.flush_events()
