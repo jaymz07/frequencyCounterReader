b6dc21ff364f9d1e2d51676932b26dae7ae4b20e
James Bounds <jamesbounds07@gmail.com>
2022-01-27 14:09:17 -0600

    Added error handling, so that program does not crash when an invalid value is recieved or serial times out.

--- scrollingPlots.py
+++ scrollingPlots.py
@@ -1,8 +1,13 @@
 import serial
 import time
+import sys
 
 import numpy as np
 import matplotlib.pyplot as plt
+
+#--------Options------------------
+accept_window = [124.5e6,125.5e6] #Set to none for no window
+scale_unit = 1e6 #MHz
 
 #--------Initialize Serial---------
 
@@ -30,7 +35,30 @@ def serDataToFloat(serData):
         count -= len(strWithoutUnit)
         out += 10**(count)*float(strWithoutUnit)
     return out, unitLabel
-        
+
+def readFreq():
+    out = None
+    while True:
+            try:
+                ser_in = ser.read_until()
+            except KeyboardInterrupt:
+                print("Tootles!")
+                sys.exit()
+            if(len(ser_in)>0):
+                try:
+                    out = serDataToFloat(ser_in)
+                except ValueError:
+                    print("Bad value from frequency counter : "+ser_in.decode())
+                    continue
+            else:
+                print("Timeout or empty value received!");
+                continue
+            if(accept_window is not None):
+                if(accept_window[0] <= out[0]*scale_unit <= accept_window[1]):
+                    return out
+                else:
+                    print("Value %.3f out of expected range of " % out[0] + str(accept_window))
+    
 
 #------------Plot----------------
 
@@ -40,21 +68,21 @@ timeStart = time.time()
 ser.read_until()
 
 for i in range(0,3):
-    data.append(serDataToFloat(ser.read_until())[0])
+    data.append(readFreq()[0]*scale_unit)
     times.append(time.time()-timeStart)
 
 fig, ax = plt.subplots(1)
 line, = ax.plot(times,data)
 
 ax.set_xlabel("Time (s)")
-ax.set_ylabel("Frequency (MHz)")
+ax.set_ylabel("Frequency (Hz)")
 fig.show()
 
 #Check when figure window is closed
 while plt.fignum_exists(1):
-    recv = serDataToFloat(ser.read_until())
+    recv = readFreq()
     print(recv)
-    data.append(recv[0])
+    data.append(recv[0]*scale_unit)
     times.append(time.time()-timeStart)
     ax.set_xlim(0,times[-1])
     ax.set_ylim(min(data),max(data))
@@ -62,3 +90,4 @@ while plt.fignum_exists(1):
     line.set_xdata(times)
     fig.canvas.draw()
     fig.canvas.flush_events()
+
